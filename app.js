7aece8de98000d77d778965c8b241843d513bf1e
RanaTaimoorAbid <96036503+RanaTaimoorAbid@users.noreply.github.com>
2024-02-22 16:38:46 +0500

    first assignment

---
+++ app.js
@@ -0,0 +1,140 @@
+//Chapter 1
+
+//Q1
+//alert("welcome")
+
+//Q2
+//alert("Error! Please enter a valid password.")
+
+//Q3
+//alert("Welcome to JS Land...\nHappy Coding!")
+
+//Q4
+//alert("Welcome to JS Land...")
+//alert("Happy Coding!")
+
+//Q5
+//console.log("Hello... I can run JS through my web brousers console")
+
+
+
+//Chapter 2
+
+//Q1
+//var username="Taimoor";
+
+//Q2
+//var Myname="Taimoor Abid";
+//document.write("My name is "+Myname)
+
+//Q3
+//var message= "Hello World"
+//alert(message)
+
+//Q4
+//var stname="Taimoor", stage="21 years old", qualification="Bachelors in CS"
+//alert(stname)
+//alert(stage)
+//alert(qualification)
+
+//Q5
+//var Pizza="PIZZA\nPIZZ\nPIZ\nPI\nP";
+//alert(Pizza)
+
+//q6
+//var email="rana@gmail.com";
+//alert("My email address is "+email)
+
+//Q7
+//var book="A smarter\nway to learn JavaScript";
+//alert("I am trying to learn from a Book "+ book)
+
+//Q8
+//document.write("Yah! I can write HTML content throught JavaScript")
+
+//Q9
+//var line="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+//alert(line)
+
+
+
+//Chapter 3
+
+//Q1
+//var age="21"
+//alert(age)
+
+//Q2
+//var noofvisit=12;
+//alert("You have visited this site " +noofvisit+ " times")
+
+//Q3
+//var Byear=2002;
+//document.write("My birth year is"+ Byear+"\nData type of my decleared variable is "+ typeof Byear)
+
+//Q4
+//var vrname="Taimoor", productT="laptop", quantity=7;
+//document.write(vrname+" ordered "+quantity+" "+productT+"(s) on XYZ Clothing store.")
+
+
+
+//Chapter 1-3
+
+//Chapter#!
+//Q1
+alert("Taimoor")
+alert("Abid")
+alert("ranataimoor0077@gmail.com")
+alert("03403659900")
+alert("oprn1234")
+
+//Q2
+alert("You're learning JavaScript!");
+
+//Q3
+alert("Hey how are you doing?")
+
+//Chapter#2
+//Q1
+var FirstName="Taimoor";
+
+//Q2
+var name
+name="Taimoor Abid";
+document.write(name);
+
+//Q3
+var teamName="team1";
+alert(teamName);
+
+//Q4
+var bestMan="Charlie";
+bestMan ="taimoor";
+document.write(bestMan);
+
+//Chapter#3
+//Q1
+var caseQty
+
+//Q2
+caseQty=144;
+
+//Q3
+var num =9;
+
+//Q4
+var sum;
+sum = caseQty+num;
+
+//Q5
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+document.write(orderTotal);
+
+//Total orderTotal is 110
+
+//Q6
+var num1=10;
+var num1=num1+2;
+document.write(num1);
